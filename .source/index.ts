7843de281ebf434b5d506bcc418257f17468d397
TheBestDeveloper <thebestdeveloper@outlook.fr>
2025-07-31 18:16:10 +0200

    feat: update documentation structure and add developer guide

--- .source/index.ts
+++ .source/index.ts
@@ -1,6 +1,6 @@
 // @ts-nocheck -- skip type checking
-import * as docs_1 from "../content/docs/test.mdx?collection=docs&hash=1753970534178"
-import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1753970534178"
+import * as docs_1 from "../content/docs/index.mdx?collection=docs&hash=1753970534178"
+import * as docs_0 from "../content/docs/developpeurs.mdx?collection=docs&hash=1753970534178"
 import { _runtime } from "fumadocs-mdx"
 import * as _source from "../source.config"
-export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"/Users/justinregulier/Desktop/Projets/ejda-chat-docs-new/content/docs/index.mdx"}, data: docs_0 }, { info: {"path":"test.mdx","absolutePath":"/Users/justinregulier/Desktop/Projets/ejda-chat-docs-new/content/docs/test.mdx"}, data: docs_1 }], [])
+export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"developpeurs.mdx","absolutePath":"/Users/justinregulier/Desktop/Projets/ejda-chat-docs-new/content/docs/developpeurs.mdx"}, data: docs_0 }, { info: {"path":"index.mdx","absolutePath":"/Users/justinregulier/Desktop/Projets/ejda-chat-docs-new/content/docs/index.mdx"}, data: docs_1 }], [{"info":{"path":"meta.json","absolutePath":"/Users/justinregulier/Desktop/Projets/ejda-chat-docs-new/content/docs/meta.json"},"data":{"pages":["index","---Développeurs---","developpeurs"]}}])
